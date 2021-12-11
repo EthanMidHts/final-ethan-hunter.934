951efd2ba062b5d2921a6fac355729e15902247d
Ethan Hunter <hunter.934@osu.edu>
2021-12-10 19:46:31 -0500

    submit

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Song.create(name: "Willow", likes:20, loves:20)
+Song.create(name: "Champagne Problmes", likes:20, loves:20)
+Song.create(name: "Gold Rush", likes:20, loves:20)
+Song.create(name: "Tis the Damn Season", likes:20, loves:20)
+Song.create(name: "Tolerate it", likes:20, loves:20)
+Song.create(name: "No Body, No Crime", likes:20, loves:20)
+Song.create(name: "Happiness", likes:20, loves:20)
+Song.create(name: "Dorothea", likes:20, loves:20)
+Song.create(name: "Coney Island", likes:20, loves:20)
+Song.create(name: "Ivy", likes:20, loves:20)
+Song.create(name: "Cowboy like Me", likes:20, loves:20)
+Song.create(name: "Long Story Short", likes:20, loves:20)
+Song.create(name: "Marjorie", likes:20, loves:20)
+Song.create(name: "Closure", likes:20, loves:20)
+Song.create(name: "Evermore", likes:20, loves:20)
+Song.create(name: "Right Where You Left Me", likes:20, loves:20)
+Song.create(name: "It's Time to Go", likes:20, loves:20)
+
